51f2d8283415adf3ecd74726a9111685a964fcd9
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-18 09:33:22 +0200

    Exercise 3.5 & 3.6 phonebook backend: post requests and error handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": "1",
@@ -26,6 +28,12 @@ let persons = [
 
 const now = new Date()
 
+const generateId = () => {
+    const id = Math.floor(Math.random() * 1000000) + 4
+    console.log(id)
+    return id
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
@@ -49,12 +57,42 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
-  
+
     response.status(204).end()
-  })
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
