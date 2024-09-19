a74df4dc74175b7cd7bbfbcf21a243c471443518
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-20 01:25:39 +0200

    Exercise 3.11 Full Stack phonebook: generate a production build of the frontend and add it to the online application

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', function (req, res) {
