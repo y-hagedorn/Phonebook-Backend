84291547ea8779e30ad388133fcc4df66e161aa4
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-20 00:11:23 +0200

    Exercise 3.9: phonebook: marriage of backend and frontend

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-morgan.token('body', function(req, res) {
+morgan.token('body', function (req, res) {
     return JSON.stringify(req.body)
 })
 
-
 let persons = [
     {
-        "id": "1",
+        "id": 1,
         "name": "Arto Hellas",
         "number": "040-123456"
     },
     {
-        "id": "2",
+        "id": 2,
         "name": "Ada Lovelace",
         "number": "39-44-5323523"
     },
     {
-        "id": "3",
+        "id": 3,
         "name": "Dan Abramov",
         "number": "12-43-234345"
     },
     {
-        "id": "4",
+        "id": 4,
         "name": "Mary Poppendieck",
         "number": "39-23-6423122"
     }
@@ -39,6 +40,10 @@ const generateId = () => {
     const id = Math.floor(Math.random() * 1000000) + 4
     console.log(id)
     return id
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.get('/', (request, response) => {
@@ -84,9 +89,9 @@ app.post('/api/persons', (request, response) => {
     }
 
     const person = {
-        name: body.name,
-        number: body.number,
         id: generateId(),
+        name: body.name,
+        number: body.number
     }
 
     persons = persons.concat(person)
@@ -95,13 +100,16 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+    const id = Number(request.params.id)
+    console.log('id to delete:', id)
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
 })
 
-const PORT = 3001
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
