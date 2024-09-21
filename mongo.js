068796499fc030ad77601f068c54ec17fdf03938
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-21 22:40:46 +0200

    Exercise 3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+if (process.argv.length === 4) {
+    console.log('node mongo.js <yourpassword> <name> <number>')
+    process.exit(1)
+}
+
+if (process.argv.length > 5) {
+    console.log('Too many arguments: node mongo.js <yourpassword> <name> <number>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.ny2lv.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person(
+    {
+        name: name,
+        number: number,
+    }
+)
+
+if (process.argv.length === 5) {
+    person.save().then(result => {
+        console.log(`added ${result.name} with number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length === 3) {
+    console.log('Phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
