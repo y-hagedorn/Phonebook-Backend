f1897412ff6cd26eef7177e5958895f4d28102cf
Yannick Hagedorn <176614986+y-hagedorn@users.noreply.github.com>
2024-09-28 01:05:26 +0200

    Exercises 3.16-3.18 Phonebook database: error handling middleware, update number, api/persons/:id and /info routes

--- index.js
+++ index.js
@@ -18,74 +18,60 @@ morgan.token('body', function (req, res) {
 })
 
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-const now = new Date()
-
+/*
 const generateId = () => {
     const id = Math.floor(Math.random() * 1000000) + 4
     console.log(id)
     return id
+}
+*/
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
 }
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+const now = new Date()
+
 app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has entries for ${persons.length} people</p><p>${now.toString()}</p>`)
+    Person.find({}).then(persons => {
+        response.send(`<p>Phonebook has entries for ${persons.length} people</p><p>${now.toString()}</p>`)
+    })
 })
+
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
-      })
+    })
 })
 
-// TODO Ex. 3.18
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
-
-/* TODO Ex. 3.17
-If the user tries to create a new phonebook entry for a person whose name is already in the phonebook, the frontend will try to update the phone number of the existing entry by making an HTTP PUT request to the entry's unique URL.
-
-Modify the backend to support this request.
-
-Verify that the frontend works after making your changes.
-*/
 
 
 app.post('/api/persons', (request, response) => {
@@ -101,11 +87,13 @@ app.post('/api/persons', (request, response) => {
             error: 'number missing'
         })
     }
+    /*
     if (persons.some(person => person.name === body.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
     }
+    */
 
     const person = new Person({
         name: body.name,
@@ -119,13 +107,29 @@ app.post('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -7,11 +7,11 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-
 mongoose.connect(url)
     .then(result => {
         console.log('connected to MongoDB')
-    }).catch(error => {
+    })
+    .catch(error => {
         console.log('error connecting to MongoDB:', error.message)
     })
 
